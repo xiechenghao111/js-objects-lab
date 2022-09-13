f05e8c70f102e4168573f3bd68ffb3b6e48725ec
xch88829051 <1152759491@qq.com>
2022-09-13 18:15:58 +0800

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,2 +1,3 @@
- ~~~ const aCar = { owner : "Joe Bloggs", type : 'Toyota Corolla 1.8', registration : '201WD1058' };
+const aCar = { owner : "Joe Bloggs", type : 'Toyota Corolla 1.8', registration : '201WD1058' };
 console.log(aCar.owner);
+
